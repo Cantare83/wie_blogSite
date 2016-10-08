07979fb9f5e363070593ab2a9233ee316acfc680
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-10-08 13:03:35 -0400

    Updated footer widgets

--- functions.php
+++ functions.php
@@ -20,30 +20,19 @@ function blank_widgets_init() {
         'description'   => 'Left widget in the footer',
         'before_widget' => '<div class="widget-footer widget-left">',
         'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>'
     ));
 
     /*--- Second Footer Widget ---*/
     register_sidebar( array(
         'name'          => ('Second Footer Widget'),
         'id'            => 'footer-two',
-        'description'   => 'Middle widget in the footer',
+        'description'   => 'Right widget in the footer',
         'before_widget' => '<div class="widget-footer widget-middle">',
         'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
-    ));
-
-    /*--- Third Footer Widget ---*/
-    register_sidebar( array(
-        'name'          => ('Third Footer Widget'),
-        'id'            => 'footer-three',
-        'description'   => 'Right widget in the footer',
-        'before_widget' => '<div class="widget-footer widget-right">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>'
     ));
 
 }
